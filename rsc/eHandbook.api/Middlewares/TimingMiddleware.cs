b777293a69740fcc86f4178000301f01bc337051
DARIEM MACIAS MORA <335286@dadeschools.net>
2024-01-17 13:57:34 -0500

    Adding custome  middleware for processing http request time.

---
+++ rsc/eHandbook.api/Middlewares/TimingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace eHandbook.api.Middlewares
+{
+    public class TimingMiddleware
+    {
+        private readonly ILogger<TimingMiddleware> _logger;
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        /// public ctr takes as expecting arguments a logger and a request delagate, this request delegate it's gonna be the nextr call.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="next"></param>
+        public TimingMiddleware(ILogger<TimingMiddleware> logger, 
+            RequestDelegate next)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext ctx) 
+        {
+            var start = DateTime.UtcNow;
+            await _next(ctx); // pass the context
+            _logger.LogInformation($"Timing sending Request {ctx.Request.Path}: {(DateTime.UtcNow - start).TotalMilliseconds} ms");
+        }
+    }
+}
