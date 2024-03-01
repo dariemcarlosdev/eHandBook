97fc35630b7f7392a49eb6208c8ec54275c36e63
DARIEM MACIAS MORA <335286@dadeschools.net>
2024-03-01 14:06:15 -0500

    targeted the property to make de property as required and create Record for ManualToDeleteDto.

--- rsc/eHandbook.Modules/eHandbook.modules.Manual/CoreDomain/DTOs/Manual/ManualToDeleteDto.cs
+++ rsc/eHandbook.Modules/eHandbook.modules.Manual/CoreDomain/DTOs/Manual/ManualToDeleteDto.cs
@@ -1,11 +1,18 @@
-﻿namespace eHandbook.modules.ManualManagement.CoreDomain.DTOs.Manual
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eHandbook.modules.ManualManagement.CoreDomain.DTOs.Manual
 {
     public class ManualToDeleteDto
     {
+        //targeted the property to make de property as required, and get respond back with an invalid request if prop.is empty.
+        [property : Required]
         public Guid Id { get; set; }
         public string? Description { get; set; }
         public string? Path { get; set; }
 
         //Add below other properties as required.
+
+        //Dto Record definition.
+        //public record ManualToDeleteDto([property : Required] Guid id, string Desciption, string Path) //targeted the property to make de property as required, and get respond back with an invalid request if prop.is empty.
     }
 }
