977d196ba26ed566bec10e01d4ed4d106fd7949e
DARIEM MACIAS MORA <335286@dadeschools.net>
2024-04-01 11:35:33 -0400

    Console App Project for IdentityServer test and configuration.

---
+++ rsc/IS/Client/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+ IdentityModel includes a client library to use with the discovery endpoint. This way you only need to know the base-address of IdentityServer - 
+ the actual endpoint addresses can be read from the metadata
+ */
+
+// discover endpoints from metadata
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    public class Program
+    {
+        private static async Task Main()
+        {
+            // discover endpoints from IdentityServer metadata
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // use the information from the discovery document to request a token to IdentityServer to access api1
+            //// request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            //// call api
+            /////To send the access token to the API you typically use the HTTP Authorization header. This is done using the SetBearerToken extension method:
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:6001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
