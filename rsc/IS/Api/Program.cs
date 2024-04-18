f2ede752193c72f938fc090e3cf057f3623f9ea6
DARIEM MACIAS MORA <335286@dadeschools.net>
2024-04-18 14:09:01 -0400

    missing }: fixed.

--- rsc/IS/Api/Program.cs
+++ rsc/IS/Api/Program.cs
@@ -1,7 +1,3 @@
-
-using Api;
-using Microsoft.AspNetCore.Authorization;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,52 +12,53 @@ builder.Services.AddAuthentication("Bearer")
                 options.ApiName = "weatherapi"; //specify api name, which is the resource tha's been protected.
                 options.ApiName = "identityApi";
 
-// adds authorization policy to make sure the token is for scope 'weatherapi'.
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("ApiScope", policy =>
-    {
-        policy.RequireAuthenticatedUser();
-        policy.RequireClaim("scope", "weatherapi.read");
-    });
-});
+                // adds authorization policy to make sure the token is for scope 'weatherapi'.
+                builder.Services.AddAuthorization(options =>
+                {
+                    options.AddPolicy("ApiScope", policy =>
+                    {
+                        policy.RequireAuthenticatedUser();
+                        policy.RequireClaim("scope", "weatherapi.read");
+                    });
+                });
+            });
+            //builder.Services.AddCors(options =>
+            //{
+            //    // this defines a CORS policy called "default"
+            //    options.AddPolicy("default", policy =>
+            //    {
+            //        policy.WithOrigins("http://localhost:5100")
+            //            .AllowAnyHeader()
+            //            .AllowAnyMethod();
+            //    });
+            //});
 
-//builder.Services.AddCors(options =>
-//{
-//    // this defines a CORS policy called "default"
-//    options.AddPolicy("default", policy =>
-//    {
-//        policy.WithOrigins("http://localhost:5100")
-//            .AllowAnyHeader()
-//            .AllowAnyMethod();
-//    });
-//});
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
 
 
-var app = builder.Build();
+            var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
-app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-app.UseRouting();
+            app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
-app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseHttpsRedirection();
 
-app.Run();
+            app.Run();
+
