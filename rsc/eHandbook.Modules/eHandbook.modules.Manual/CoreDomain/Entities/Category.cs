8c5632f208ad11e045ceb3d315334aed93e0fa1a
DARIEM MACIAS MORA <335286@dadeschools.net>
2024-04-18 14:51:41 -0400

    Updating Category Entity model.

--- rsc/eHandbook.Modules/eHandbook.modules.Manual/CoreDomain/Entities/Category.cs
+++ rsc/eHandbook.Modules/eHandbook.modules.Manual/CoreDomain/Entities/Category.cs
@@ -1,11 +1,22 @@
 ﻿using eHandbook.Core.Domain;
+using eHandbook.Core.Domain.Abstractions;
 
 namespace eHandbook.modules.ManualManagement.CoreDomain.Entities
 {
-    public class Category : BaseEntity
+    public class Category : BaseEntity , IAuditableEntity
     {
+   
         string CategoryName { get; set; }
         string CategoryDescription { get; set; }
+
+        public string? CreatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? CreatedOn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? UpdatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? UpdatedOn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsUpdated { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? DeletedOn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string? DeletedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsDeleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
     }
 }
