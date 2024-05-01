ee53e385adee6a4de6249388fb9abd307b5cd9b7
DARIEM MACIAS MORA <335286@dadeschools.net>
2024-05-01 10:56:13 -0400

    Creating my CustomValidationException class extending from ValidationException.

---
+++ rsc/eHandbook.Infrastructure/Utilities/Validations/MyCustomInputValidationException.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System.Runtime.Serialization;
+
+namespace eHandbook.Infrastructure.Utilities.Validations
+{
+    [Serializable]
+    internal class MyCustomInputValidationException : ValidationException
+    {
+        public MyCustomInputValidationException(string message) : base(message)
+        {
+        }
+
+        public MyCustomInputValidationException(IEnumerable<ValidationFailure> errors) : base(errors)
+        {
+        }
+
+        public MyCustomInputValidationException(string message, IEnumerable<ValidationFailure> errors) : base(message, errors)
+        {
+        }
+
+        public MyCustomInputValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public MyCustomInputValidationException(string message, IEnumerable<ValidationFailure> errors, bool appendDefaultMessage) : base(message, errors, appendDefaultMessage)
+        {
+        }
+    }
+}
