78bd8c04fb71bd10e9ebcee095d62f735e83aecb
Dariem Carlos Macias Mora <31046315+dariemcarlosdev@users.noreply.github.com>
2024-06-25 15:36:14 -0400

    #fix marks properties in dto class #ManualDTO as public.

--- rsc/eHandbook.Modules/eHandbook.modules.Manual/CoreDomain/DTOs/Manual/ManualDto.cs
+++ rsc/eHandbook.Modules/eHandbook.modules.Manual/CoreDomain/DTOs/Manual/ManualDto.cs
@@ -5,14 +5,12 @@ namespace eHandbook.modules.ManualManagement.CoreDomain.DTOs.Manual
 {
     public class ManualDto
     {
-        //Taking out Id from ManualDto since this is not revelant for users, for security reasons.
-        //public Guid Id { get; set; }
         [Required]
         public string? Description { get; set; }
         [Required]
         //include regular expretion for path validation.
         public string? Path { get; set; }
-        internal EntityDetailsDto? AuditableDetails { get; set; }
-        internal List<CategoryEntity>? Categories { get; set; }
+        public EntityDetailsDto? AuditableDetails { get; set; }
+        public List<CategoryEntity>? Categories { get; set; }
     }
 }
