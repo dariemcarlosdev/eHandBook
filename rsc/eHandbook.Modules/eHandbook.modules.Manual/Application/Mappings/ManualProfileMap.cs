703a3e9f12d4e2ed9b8cd0df09bd5fe573346187
Dariem Carlos Macias Mora <31046315+dariemcarlosdev@users.noreply.github.com>
2024-06-25 15:35:18 -0400

    #fix ManualProfileMap to map properties and initialize nested object.

--- rsc/eHandbook.Modules/eHandbook.modules.Manual/Application/Mappings/ManualProfileMap.cs
+++ rsc/eHandbook.Modules/eHandbook.modules.Manual/Application/Mappings/ManualProfileMap.cs
@@ -17,7 +17,23 @@ namespace eHandbook.modules.ManualManagement.Application.Mappings
         public ManualProfileMap()
         {
             //In this case both POCO objects have same same, in case of diferents props. name, the props. need to be mapped as well.(http://tinyurl.com/yremtj9b)
-            CreateMap<ManualEntity, ManualDto>().ReverseMap();
+            CreateMap<ManualEntity, ManualDto>()
+                // you should use the .ForPath method instead of .ForMember. The .ForPath method is designed for scenarios where you need to map to a nested property.
+                .ForPath(dest => dest.AuditableDetails.CreatedBy, opt => opt.MapFrom(src => src.CreatedBy))
+                .ForPath(dest => dest.AuditableDetails.CreatedOn, opt => opt.MapFrom(src => src.CreatedOn))
+                .ForPath(dest => dest.AuditableDetails.UpdatedBy, opt => opt.MapFrom(src => src.UpdatedBy))
+                .ForPath(dest => dest.AuditableDetails.UpdatedOn, opt => opt.MapFrom(src => src.UpdatedOn))
+                .ForPath(dest => dest.AuditableDetails.IsUpdated, opt => opt.MapFrom(src => src.IsUpdated))
+                .ForPath(dest => dest.AuditableDetails.DeletedOn, opt => opt.MapFrom(src => src.DeletedOn))
+                .ForPath(dest => dest.AuditableDetails.DeletedBy, opt => opt.MapFrom(src => src.DeletedBy))
+
+                //configuring AutoMapper to initialize the nested EntityDetailsDto object if it's null. 
+                .AfterMap((src, dest) => {
+                    if (dest.AuditableDetails == null)
+                    {
+                        dest.AuditableDetails = new EntityDetailsDto();
+                    }
+                }).ReverseMap();
         }
     }
 }
