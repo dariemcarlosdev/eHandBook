0554d09c28af0ca7de1233d0b37e121fd74ed5ec
Dariem Carlos Macias Mora <31046315+dariemcarlosdev@users.noreply.github.com>
2024-07-01 09:39:22 -0400

    transformation of the `AuditableDetailsDto` class into a record with positional parameters, aimed at enhancing code readability and simplifying the definition of immutable data types. No implement IAuditable Interface, since now it is inmutable, but contain all their properties.

--- rsc/eHandbook.Modules/eHandbook.modules.Manual/CoreDomain/DTOs/Manual/AuditableDetailsDto.cs
+++ rsc/eHandbook.Modules/eHandbook.modules.Manual/CoreDomain/DTOs/Manual/AuditableDetailsDto.cs
@@ -2,5 +2,12 @@
 
 namespace eHandbook.modules.ManualManagement.CoreDomain.DTOs.Manual
 {
-    public record AuditableDetailsDto(string? CreatedBy, DateTime? CreatedOn, string? UpdatedBy, DateTime? UpdatedOn, bool IsUpdated, DateTime? DeletedOn, string? DeletedBy, bool IsDeleted) : IAuditableEntity;
+    public record AuditableDetailsDto(string? CreatedBy,
+                                      DateTime? CreatedOn,
+                                      string? UpdatedBy,
+                                      DateTime? UpdatedOn,
+                                      bool IsUpdated,
+                                      DateTime? DeletedOn,
+                                      string? DeletedBy,
+                                      bool IsDeleted);
 }
