4371b4c708fc3cd7a62dd37e56d22b80860baed6
Dariem Carlos Macias Mora <31046315+dariemcarlosdev@users.noreply.github.com>
2024-07-02 08:42:54 -0400

    #refactoring #configuration adding comment.

--- rsc/eHandbook.Modules/eHandbook.modules.Manual/CoreDomain/DTOs/Manual/AuditableDetailsDto.cs
+++ rsc/eHandbook.Modules/eHandbook.modules.Manual/CoreDomain/DTOs/Manual/AuditableDetailsDto.cs
@@ -2,12 +2,22 @@
 
 namespace eHandbook.modules.ManualManagement.CoreDomain.DTOs.Manual
 {
-    public record AuditableDetailsDto(string? CreatedBy,
-                                      DateTime? CreatedOn,
-                                      string? UpdatedBy,
-                                      DateTime? UpdatedOn,
-                                      bool IsUpdated,
-                                      DateTime? DeletedOn,
-                                      string? DeletedBy,
-                                      bool IsDeleted);
+    //convert to read-init properties
+    /// <summary>
+    /// 
+    /// </summary>
+    public record AuditableDetailsDto
+    {
+        //adding init accessors to the properties will explicitly mark them as init-only, which allows them to be set during object initialization.
+        //init accessors are designed to make objects immutable after initialization(cannot be modified after creation).
+        //This resolves the issue of assigning values to their properties outside of the object initializer or constructor, which is not allowed for init props.
+        public string? CreatedBy { get; init; }
+        public DateTime? CreatedOn { get; init; }
+        public string? UpdatedBy { get; init; }
+        public DateTime? UpdatedOn { get; init; }
+        public bool IsUpdated { get; init; }
+        public DateTime? DeletedOn { get; init; }
+        public string? DeletedBy { get; init; }
+        public bool IsDeleted { get; init; }
+    }
 }
